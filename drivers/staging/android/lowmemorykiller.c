329866106313fc34feae9b1908c6c3ebd7ae9ced
Dennis Rassmann <showp1984@gmail.com>
2012-10-03 17:57:18 +0200

    drivers: staging: android: lmk compile fixup
    Change-Id: I9c970f6b0b16df4bbb6c686d2d72bf038897e327

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -74,7 +74,7 @@ static int lmk_fast_run = 1;
 
 static unsigned long lowmem_deathpending_timeout;
 
-extern int compact_nodes();
+extern int compact_nodes(bool sync);
 
 #define lowmem_print(level, x...)			\
 	do {						\
