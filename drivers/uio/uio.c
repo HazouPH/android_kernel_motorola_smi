64dae225520fd4cc42d642a8acab342d5e039e55
Michal Hocko <mhocko@suse.cz>
2013-11-06 14:44:37 +0100

    - patches.fixes/Fix-a-few-incorrectly-checked-io_-remap_pfn_range-ca.patch:   Fix a few incorrectly checked [io_]remap_pfn_range() calls   (bnc#849021, CVE-2013-4511).
    suse-commit: 69b0eb05fe891ae932d800fb2aca2d3a41d8e75f

--- drivers/uio/uio.c
+++ drivers/uio/uio.c
@@ -651,7 +651,13 @@ static int uio_mmap_physical(struct vm_area_struct *vma)
 {
 	struct uio_device *idev = vma->vm_private_data;
 	int mi = uio_find_mem_index(vma);
+	struct uio_mem *mem;
 	if (mi < 0)
+		return -EINVAL;
+
+	mem = idev->info->mem + mi;
+
+	if (vma->vm_end - vma->vm_start > mem->size)
 		return -EINVAL;
 
 	vma->vm_flags |= VM_IO | VM_RESERVED;
@@ -660,7 +666,7 @@ static int uio_mmap_physical(struct vm_area_struct *vma)
 
 	return remap_pfn_range(vma,
 			       vma->vm_start,
-			       idev->info->mem[mi].addr >> PAGE_SHIFT,
+			       mem->addr >> PAGE_SHIFT,
 			       vma->vm_end - vma->vm_start,
 			       vma->vm_page_prot);
 }
