4a6244fe9ed1308d8e23ac6ef90755f32cfe2e77
Michal Kubecek <mkubecek@suse.cz>
2014-03-03 10:19:19 +0100

    - inet: fix possible memory corruption with UDP_CORK and UFO   (bnc#847672 CVE-2013-4470). - Refresh   patches.fixes/net-add-APIs-for-manipulating-skb-page-fragments.patch.
    Change-Id: Ied1af30f7c79d8bda01ba2f42db3186b2ac82bc4
    suse-commit: 18419b8541bc5d95a82ef2a217129364a25978e5

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1113,6 +1113,11 @@ static inline int skb_pagelen(const struct sk_buff *skb)
 	return len + skb_headlen(skb);
 }
 
+static inline bool skb_has_frags(const struct sk_buff *skb)
+{
+	return skb_shinfo(skb)->nr_frags;
+}
+
 static inline void skb_fill_page_desc(struct sk_buff *skb, int i,
 				      struct page *page, int off, int size)
 {
