a32af85e054171b2c79f37b19cc912cc78f6e43e
Ben Hutchings <ben@decadent.org.uk>
2016-01-19 21:35:15 +0000

    BACKPORT: perf tools: Document the perf sysctls
    perf_event_paranoid was only documented in source code and a perf error
    message.  Copy the documentation from the error message to
    Documentation/sysctl/kernel.txt.
    
    Conflicts:
    	Documentation/sysctl/kernel.txt
    	tools/perf/util/evsel.c
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: linux-doc@vger.kernel.org
    Link: http://lkml.kernel.org/r/20160119213515.GG2637@decadent.org.uk
    [ Remove reference to external Documentation file, provide info inline, as before ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Bug: 29054680
    Bug: 29119870
    Signed-off-by: Dennis Cagle <d-cagle@codeaurora.org>
    Change-Id: I13e73cfb2ad761c94762d0c8196df7725abdf5c5
    (cherry picked from commit 746938f9d97d74f6c2833a0ede49506bdfcd89e4)

--- tools/perf/util/debug.c
+++ tools/perf/util/debug.c
@@ -59,12 +59,15 @@ void ui__warning(const char *format, ...)
 
 void ui__warning_paranoid(void)
 {
-	ui__warning("Permission error - are you root?\n"
+	ui__warning("Permission error - are you root?\n\n"
 		    "Consider tweaking /proc/sys/kernel/perf_event_paranoid:\n"
-		    " -1 - Not paranoid at all\n"
-		    "  0 - Disallow raw tracepoint access for unpriv\n"
-		    "  1 - Disallow cpu events for unpriv\n"
-		    "  2 - Disallow kernel profiling for unpriv\n");
+		    "which controls use of the performance events system by\n"
+		    "unprivileged users (without CAP_SYS_ADMIN).\n\n"
+		    "The default value is 1:\n\n"
+		    "  -1: Allow use of (almost) all events by all users\n"
+		    ">= 0: Disallow raw tracepoint access by users without CAP_IOC_LOCK\n"
+		    ">= 1: Disallow CPU event access by users without CAP_SYS_ADMIN\n"
+		    ">= 2: Disallow kernel profiling by users without CAP_SYS_ADMIN\n");
 }
 
 void trace_event(union perf_event *event)
