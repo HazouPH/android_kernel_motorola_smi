0003a050a05938ef5dc143533465c9e74d56b573
nadlabak <pavel@doshaska.net>
2016-06-05 01:32:37 +0200

    f2fs: kernel 3.0: no reason param for writeback_inodes_sb
    Change-Id: I5dff144393c8a8693aebce66062487e40d6be6d6

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -950,7 +950,7 @@ static int f2fs_remount(struct super_block *sb, int *flags, char *data)
 	}
 
 	if (*flags & MS_RDONLY) {
-		writeback_inodes_sb(sb, WB_REASON_SYNC);
+		writeback_inodes_sb(sb);
 		sync_inodes_sb(sb);
 
 		set_sbi_flag(sbi, SBI_IS_DIRTY);
