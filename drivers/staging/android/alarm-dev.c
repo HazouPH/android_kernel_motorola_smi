529b8788f8ff0bc0c8c5124d2f13a848cbaf50b9
Patrick Harbers <jgrharbers@gmail.com>
2018-03-17 22:15:27 +0100

    Revert "staging: android-alarm: Switch from wakelocks to wakeup sources"
    This reverts commit a4a3a8e5cce46beb6581440700e342967863f3f4.
    
    Change-Id: I35e5387367040ca469490a1d14487ff01fbd0671

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -45,7 +45,7 @@ do {									\
 
 static int alarm_opened;
 static DEFINE_SPINLOCK(alarm_slock);
-static struct wakeup_source alarm_wake_lock;
+static struct wake_lock alarm_wake_lock;
 static DECLARE_WAIT_QUEUE_HEAD(alarm_wait_queue);
 static uint32_t alarm_pending;
 static uint32_t alarm_enabled;
@@ -104,7 +104,7 @@ static void alarm_clear(enum android_alarm_type alarm_type)
 	if (alarm_pending) {
 		alarm_pending &= ~alarm_type_mask;
 		if (!alarm_pending && !wait_pending)
-			__pm_relax(&alarm_wake_lock);
+			wake_unlock(&alarm_wake_lock);
 	}
 	alarm_enabled &= ~alarm_type_mask;
 	spin_unlock_irqrestore(&alarm_slock, flags);
@@ -133,7 +133,7 @@ static int alarm_wait(void)
 	spin_lock_irqsave(&alarm_slock, flags);
 	alarm_dbg(IO, "alarm wait\n");
 	if (!alarm_pending && wait_pending) {
-		__pm_relax(&alarm_wake_lock);
+		wake_unlock(&alarm_wake_lock);
 		wait_pending = 0;
 	}
 	spin_unlock_irqrestore(&alarm_slock, flags);
@@ -340,7 +340,7 @@ static int alarm_release(struct inode *inode, struct file *file)
 			if (alarm_pending)
 				alarm_dbg(INFO, "%s: clear pending alarms %x\n",
 					  __func__, alarm_pending);
-			__pm_relax(&alarm_wake_lock);
+			wake_unlock(&alarm_wake_lock);
 			wait_pending = 0;
 			alarm_pending = 0;
 		}
@@ -358,7 +358,7 @@ static void devalarm_triggered(struct devalarm *alarm)
 	alarm_dbg(INT, "%s: type %d\n", __func__, alarm->type);
 	spin_lock_irqsave(&alarm_slock, flags);
 	if (alarm_enabled & alarm_type_mask) {
-		__pm_wakeup_event(&alarm_wake_lock, 5000); /* 5secs */
+		wake_lock_timeout(&alarm_wake_lock, 5 * HZ);
 		alarm_enabled &= ~alarm_type_mask;
 		alarm_pending |= alarm_type_mask;
 		wake_up(&alarm_wait_queue);
@@ -427,14 +427,15 @@ static int __init alarm_dev_init(void)
 			alarms[i].u.hrt.function = devalarm_hrthandler;
 	}
 
-	wakeup_source_init(&alarm_wake_lock, "alarm");
+	wake_lock_init(&alarm_wake_lock, WAKE_LOCK_SUSPEND, "alarm");
+
 	return 0;
 }
 
 static void  __exit alarm_dev_exit(void)
 {
 	misc_deregister(&alarm_device);
-	wakeup_source_trash(&alarm_wake_lock);
+	wake_lock_destroy(&alarm_wake_lock);
 }
 
 module_init(alarm_dev_init);
