LineageOS/android_kernel_oneplus_msm8996

sdcardfs: Add support for parsing Android's package list
 * On M, we don't have support to set up the packages in configfs,
   so bring back the packages parser.
 * Fix up some debug prints too.

Change-Id: I4e0cb053e9702e0d0e0324917f2046bd5fb471ca

fs/sdcardfs/packagelist.c
@@ -19,17 +19,26 @@
  */
 
 #include "sdcardfs.h"
-#include <linux/hashtable.h>
 #include <linux/delay.h>
-
-
+#include <linux/hashtable.h>
 #include <linux/init.h>
+#include <linux/inotify.h>
+#include <linux/kthread.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/syscalls.h>
 
 #include <linux/configfs.h>
 
 #define STRING_BUF_SIZE		(512)
+
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+/* Path to system-provided mapping of package name to appIds */
+static const char* const kpackageslist_file = "/data/system/packages.list";
+
+/* Supplementary groups to execute with */
+static const gid_t kgroups[1] = { AID_PACKAGE_INFO };
+#endif
 
 struct hashtable_entry {
 	struct hlist_node hlist;
@@ -45,7 +54,13 @@ struct sb_list {
 struct packagelist_data {
 	DECLARE_HASHTABLE(package_to_appid,8);
 	struct mutex hashtable_lock;
-
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+	struct task_struct *thread_id;
+#endif
+	char read_buf[STRING_BUF_SIZE];
+	char event_buf[STRING_BUF_SIZE];
+	char app_name_buf[STRING_BUF_SIZE];
+	char gids_buf[STRING_BUF_SIZE];
 };
 
 static struct packagelist_data *pkgl_data_all;
@@ -197,17 +212,177 @@ static void remove_str_to_int(struct packagelist_data *pkgl_dat, const char *key
 	return;
 }
 
-static void remove_all_hashentrys(struct packagelist_data *pkgl_dat)
+static void remove_all_hashentries_locked(struct packagelist_data *pkgl_dat)
 {
 	struct hashtable_entry *hash_cur;
 	struct hlist_node *h_t;
 	int i;
-	mutex_lock(&pkgl_dat->hashtable_lock);
 	hash_for_each_safe(pkgl_dat->package_to_appid, i, h_t, hash_cur, hlist)
 		remove_str_to_int_lock(hash_cur);
-	mutex_unlock(&pkgl_dat->hashtable_lock);
 	hash_init(pkgl_dat->package_to_appid);
 }
+
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+static int read_package_list(struct packagelist_data *pkgl_dat) {
+
+	int ret;
+	int fd;
+	int read_amount;
+
+	printk(KERN_INFO "sdcardfs: read_package_list\n");
+
+	mutex_lock(&pkgl_dat->hashtable_lock);
+
+	remove_all_hashentries_locked(pkgl_dat);
+
+	fd = sys_open(kpackageslist_file, O_RDONLY, 0);
+	if (fd < 0) {
+		printk(KERN_ERR "sdcardfs: failed to open package list\n");
+		mutex_unlock(&pkgl_dat->hashtable_lock);
+		return fd;
+	}
+
+	while ((read_amount = sys_read(fd, pkgl_dat->read_buf,
+					sizeof(pkgl_dat->read_buf))) > 0) {
+		int appid;
+		int one_line_len = 0;
+		int additional_read;
+
+		while (one_line_len < read_amount) {
+			if (pkgl_dat->read_buf[one_line_len] == '\n') {
+				one_line_len++;
+				break;
+			}
+			one_line_len++;
+		}
+		additional_read = read_amount - one_line_len;
+		if (additional_read > 0)
+			sys_lseek(fd, -additional_read, SEEK_CUR);
+
+		if (sscanf(pkgl_dat->read_buf, "%s %d %*d %*s %*s %s",
+				pkgl_dat->app_name_buf, &appid,
+				pkgl_dat->gids_buf) == 3) {
+			ret = insert_str_to_int_lock(pkgl_dat, pkgl_dat->app_name_buf, appid);
+			if (ret) {
+				sys_close(fd);
+				mutex_unlock(&pkgl_dat->hashtable_lock);
+				return ret;
+			}
+		}
+	}
+
+	sys_close(fd);
+	mutex_unlock(&pkgl_dat->hashtable_lock);
+	return 0;
+}
+
+static int packagelist_reader(void *thread_data)
+{
+	struct packagelist_data *pkgl_dat = (struct packagelist_data *)thread_data;
+	struct inotify_event *event;
+	bool active = false;
+	int event_pos;
+	int event_size;
+	int res = 0;
+	int nfd;
+
+	allow_signal(SIGINT);
+
+	nfd = sys_inotify_init();
+	if (nfd < 0) {
+		printk(KERN_ERR "sdcardfs: inotify_init failed: %d\n", nfd);
+		return nfd;
+	}
+
+	while (!kthread_should_stop()) {
+		if (signal_pending(current)) {
+			ssleep(1);
+			continue;
+		}
+
+		if (!active) {
+			res = sys_inotify_add_watch(nfd, kpackageslist_file, IN_DELETE_SELF);
+			if (res < 0) {
+				if (res == -ENOENT || res == -EACCES) {
+				/* Framework may not have created yet, sleep and retry */
+					printk(KERN_ERR "sdcardfs: missing packages.list; retrying\n");
+					ssleep(2);
+					printk(KERN_ERR "sdcardfs: missing packages.list_end; retrying\n");
+					continue;
+				} else {
+					printk(KERN_ERR "sdcardfs: inotify_add_watch failed: %d\n", res);
+					goto interruptable_sleep;
+				}
+			}
+			/* Watch above will tell us about any future changes, so
+			 * read the current state. */
+			res = read_package_list(pkgl_dat);
+			if (res) {
+				printk(KERN_ERR "sdcardfs: read_package_list failed: %d\n", res);
+				goto interruptable_sleep;
+			}
+			active = true;
+		}
+
+		event_pos = 0;
+		res = sys_read(nfd, pkgl_dat->event_buf, sizeof(pkgl_dat->event_buf));
+		if (res < (int) sizeof(*event)) {
+			if (res == -EINTR)
+				continue;
+			printk(KERN_ERR "sdcardfs: failed to read inotify event: %d\n", res);
+			goto interruptable_sleep;
+		}
+
+		while (res >= (int) sizeof(*event)) {
+			event = (struct inotify_event *) (pkgl_dat->event_buf + event_pos);
+
+			printk(KERN_INFO "sdcardfs: inotify event: %08x\n", event->mask);
+			if ((event->mask & IN_IGNORED) == IN_IGNORED) {
+				/* Previously watched file was deleted, probably due to move
+				 * that swapped in new data; re-arm the watch and read. */
+				active = false;
+			}
+
+			event_size = sizeof(*event) + event->len;
+			res -= event_size;
+			event_pos += event_size;
+		}
+		continue;
+
+interruptable_sleep:
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+	}
+	flush_signals(current);
+	sys_close(nfd);
+	return res;
+}
+
+struct packagelist_data * package_reader_create(struct packagelist_data *pkgl_dat)
+{
+	struct task_struct *packagelist_thread;
+
+	packagelist_thread = kthread_run(packagelist_reader, (void *)pkgl_dat, "pkgld");
+	if (IS_ERR(packagelist_thread)) {
+		printk(KERN_ERR "sdcardfs: creating kthread failed\n");
+		kfree(pkgl_dat);
+		goto out;
+	}
+	pkgl_dat->thread_id = packagelist_thread;
+
+	printk(KERN_INFO "sdcardfs: created packagelist pkgld/%d\n",
+				(int)pkgl_dat->thread_id->pid);
+
+out:
+	return pkgl_dat;
+}
+
+void package_reader_destroy(struct packagelist_data * pkgl_dat)
+{
+	force_sig_info(SIGINT, SEND_SIG_PRIV, pkgl_dat->thread_id);
+	kthread_stop(pkgl_dat->thread_id);
+}
+#endif
 
 static struct packagelist_data * packagelist_create(void)
 {
@@ -215,19 +390,28 @@ static struct packagelist_data * packagelist_create(void)
 
 	pkgl_dat = kmalloc(sizeof(*pkgl_dat), GFP_KERNEL | __GFP_ZERO);
 	if (!pkgl_dat) {
-                printk(KERN_ERR "sdcardfs: Failed to create hash\n");
+		printk(KERN_ERR "sdcardfs: Failed to create hash\n");
 		return ERR_PTR(-ENOMEM);
 	}
 
 	mutex_init(&pkgl_dat->hashtable_lock);
 	hash_init(pkgl_dat->package_to_appid);
 
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+	return package_reader_create(pkgl_dat);
+#else
 	return pkgl_dat;
+#endif
 }
 
 static void packagelist_destroy(struct packagelist_data *pkgl_dat)
 {
-	remove_all_hashentrys(pkgl_dat);
+	mutex_lock(&pkgl_dat->hashtable_lock);
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+	package_reader_destroy(pkgl_dat);
+#endif
+	remove_all_hashentries_locked(pkgl_dat);
+	mutex_unlock(&pkgl_dat->hashtable_lock);
 	printk(KERN_INFO "sdcardfs: destroyed packagelist pkgld\n");
 	kfree(pkgl_dat);
 }
@@ -435,7 +619,7 @@ int packagelist_init(void)
 
 	pkgl_data_all = packagelist_create();
 	configfs_sdcardfs_init();
-        return 0;
+	return 0;
 }
 
 void packagelist_exit(void)

HazouPH/android_kernel_motorola_smi

fs/sdcardfs/packagelist.c
@@ -19,17 +19,26 @@
  */
 
 #include "sdcardfs.h"
-#include "linux/hashtable.h"
 #include <linux/delay.h>
-
-
+#include <linux/hashtable.h>
 #include <linux/init.h>
+#include <linux/inotify.h>
+#include <linux/kthread.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/syscalls.h>
 
 #include <linux/configfs.h>
 
 #define STRING_BUF_SIZE		(512)
+
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+/* Path to system-provided mapping of package name to appIds */
+static const char* const kpackageslist_file = "/data/system/packages.list";
+
+/* Supplementary groups to execute with */
+static const gid_t kgroups[1] = { AID_PACKAGE_INFO };
+#endif
 
 struct hashtable_entry {
         struct hlist_node hlist;
@@ -45,6 +54,13 @@ struct sb_list {
 struct packagelist_data {
 	DECLARE_HASHTABLE(package_to_appid,8);
 	spinlock_t hashtable_lock;
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+	struct task_struct *thread_id;
+#endif
+	char read_buf[STRING_BUF_SIZE];
+	char event_buf[STRING_BUF_SIZE];
+	char app_name_buf[STRING_BUF_SIZE];
+	char gids_buf[STRING_BUF_SIZE];
 };
 
 static struct packagelist_data *pkgl_data_all;
@@ -197,20 +213,178 @@ static void remove_str_to_int(struct packagelist_data *pkgl_dat, const char *key
 	return;
 }
 
-static void remove_all_hashentrys(struct packagelist_data *pkgl_dat)
+static void remove_all_hashentries_locked(struct packagelist_data *pkgl_dat)
 {
 	struct hashtable_entry *hash_cur;
 	struct hlist_node *h_n;
 	struct hlist_node *h_t;
 	int i;
-
-	spin_lock(&pkgl_data_all->hashtable_lock);
 	hash_for_each_safe(pkgl_dat->package_to_appid, i, h_t, h_n, hash_cur, hlist)
                 remove_str_to_int_lock(hash_cur);
-	spin_unlock(&pkgl_data_all->hashtable_lock);
-
 	hash_init(pkgl_dat->package_to_appid);
 }
+
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+static int read_package_list(struct packagelist_data *pkgl_dat) {
+
+	int ret;
+	int fd;
+	int read_amount;
+
+	printk(KERN_INFO "sdcardfs: read_package_list\n");
+
+	spin_lock(&pkgl_dat->hashtable_lock);
+
+	remove_all_hashentries_locked(pkgl_dat);
+
+	fd = sys_open(kpackageslist_file, O_RDONLY, 0);
+	if (fd < 0) {
+		printk(KERN_ERR "sdcardfs: failed to open package list\n");
+		spin_unlock(&pkgl_dat->hashtable_lock);
+		return fd;
+	}
+
+	while ((read_amount = sys_read(fd, pkgl_dat->read_buf,
+					sizeof(pkgl_dat->read_buf))) > 0) {
+		int appid;
+		int one_line_len = 0;
+		int additional_read;
+
+		while (one_line_len < read_amount) {
+			if (pkgl_dat->read_buf[one_line_len] == '\n') {
+				one_line_len++;
+				break;
+			}
+			one_line_len++;
+		}
+		additional_read = read_amount - one_line_len;
+		if (additional_read > 0)
+			sys_lseek(fd, -additional_read, SEEK_CUR);
+
+		if (sscanf(pkgl_dat->read_buf, "%s %d %*d %*s %*s %s",
+				pkgl_dat->app_name_buf, &appid,
+				pkgl_dat->gids_buf) == 3) {
+			ret = insert_str_to_int_lock(pkgl_dat, pkgl_dat->app_name_buf, appid);
+			if (ret) {
+				sys_close(fd);
+				spin_unlock(&pkgl_dat->hashtable_lock);
+				return ret;
+			}
+		}
+	}
+
+	sys_close(fd);
+	spin_unlock(&pkgl_dat->hashtable_lock);
+	return 0;
+}
+
+static int packagelist_reader(void *thread_data)
+{
+	struct packagelist_data *pkgl_dat = (struct packagelist_data *)thread_data;
+	struct inotify_event *event;
+	bool active = false;
+	int event_pos;
+	int event_size;
+	int res = 0;
+	int nfd;
+
+	allow_signal(SIGINT);
+
+	nfd = sys_inotify_init();
+	if (nfd < 0) {
+		printk(KERN_ERR "sdcardfs: inotify_init failed: %d\n", nfd);
+		return nfd;
+	}
+
+	while (!kthread_should_stop()) {
+		if (signal_pending(current)) {
+			ssleep(1);
+			continue;
+		}
+
+		if (!active) {
+			res = sys_inotify_add_watch(nfd, kpackageslist_file, IN_DELETE_SELF);
+			if (res < 0) {
+				if (res == -ENOENT || res == -EACCES) {
+				/* Framework may not have created yet, sleep and retry */
+					printk(KERN_ERR "sdcardfs: missing packages.list; retrying\n");
+					ssleep(2);
+					printk(KERN_ERR "sdcardfs: missing packages.list_end; retrying\n");
+					continue;
+				} else {
+					printk(KERN_ERR "sdcardfs: inotify_add_watch failed: %d\n", res);
+					goto interruptable_sleep;
+				}
+			}
+			/* Watch above will tell us about any future changes, so
+			 * read the current state. */
+			res = read_package_list(pkgl_dat);
+			if (res) {
+				printk(KERN_ERR "sdcardfs: read_package_list failed: %d\n", res);
+				goto interruptable_sleep;
+			}
+			active = true;
+		}
+
+		event_pos = 0;
+		res = sys_read(nfd, pkgl_dat->event_buf, sizeof(pkgl_dat->event_buf));
+		if (res < (int) sizeof(*event)) {
+			if (res == -EINTR)
+				continue;
+			printk(KERN_ERR "sdcardfs: failed to read inotify event: %d\n", res);
+			goto interruptable_sleep;
+		}
+
+		while (res >= (int) sizeof(*event)) {
+			event = (struct inotify_event *) (pkgl_dat->event_buf + event_pos);
+
+			printk(KERN_INFO "sdcardfs: inotify event: %08x\n", event->mask);
+			if ((event->mask & IN_IGNORED) == IN_IGNORED) {
+				/* Previously watched file was deleted, probably due to move
+				 * that swapped in new data; re-arm the watch and read. */
+				active = false;
+			}
+
+			event_size = sizeof(*event) + event->len;
+			res -= event_size;
+			event_pos += event_size;
+		}
+		continue;
+
+interruptable_sleep:
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule();
+	}
+	flush_signals(current);
+	sys_close(nfd);
+	return res;
+}
+
+struct packagelist_data * package_reader_create(struct packagelist_data *pkgl_dat)
+{
+	struct task_struct *packagelist_thread;
+
+	packagelist_thread = kthread_run(packagelist_reader, (void *)pkgl_dat, "pkgld");
+	if (IS_ERR(packagelist_thread)) {
+		printk(KERN_ERR "sdcardfs: creating kthread failed\n");
+		kfree(pkgl_dat);
+		goto out;
+	}
+	pkgl_dat->thread_id = packagelist_thread;
+
+	printk(KERN_INFO "sdcardfs: created packagelist pkgld/%d\n",
+				(int)pkgl_dat->thread_id->pid);
+
+out:
+	return pkgl_dat;
+}
+
+void package_reader_destroy(struct packagelist_data * pkgl_dat)
+{
+	force_sig_info(SIGINT, SEND_SIG_PRIV, pkgl_dat->thread_id);
+	kthread_stop(pkgl_dat->thread_id);
+}
+#endif
 
 static struct packagelist_data * packagelist_create(void)
 {
@@ -218,20 +392,27 @@ static struct packagelist_data * packagelist_create(void)
 
 	pkgl_dat = kmalloc(sizeof(*pkgl_dat), GFP_KERNEL | __GFP_ZERO);
 	if (!pkgl_dat) {
-               printk(KERN_ERR "sdcardfs: Failed to create hash\n");
+		printk(KERN_ERR "sdcardfs: Failed to create hash\n");
 		return ERR_PTR(-ENOMEM);
 	}
 
 	spin_lock_init(&pkgl_dat->hashtable_lock);
 	hash_init(pkgl_dat->package_to_appid);
 
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+	return package_reader_create(pkgl_dat);
+#else
 	return pkgl_dat;
+#endif
 }
 
 static void packagelist_destroy(struct packagelist_data *pkgl_dat)
 {
 	spin_lock(&pkgl_dat->hashtable_lock);
-	remove_all_hashentrys(pkgl_dat);
+#ifdef CONFIG_SDCARD_FS_ANDROID_PKGLIST
+	package_reader_destroy(pkgl_dat);
+#endif
+	remove_all_hashentries_locked(pkgl_dat);
 	spin_unlock(&pkgl_dat->hashtable_lock);
 	printk(KERN_INFO "sdcardfs: destroyed packagelist pkgld\n");
 	kfree(pkgl_dat);
@@ -440,7 +621,7 @@ int packagelist_init(void)
 
 	pkgl_data_all = packagelist_create();
 	configfs_sdcardfs_init();
-        return 0;
+	return 0;
 }
 
 void packagelist_exit(void)